ddae9da1ed57a01e06c46ae1dee6f6b4977cd187
Sahil Sao <51815576+sahilsao@users.noreply.github.com>
2020-02-17 14:18:14 +0530

    Add files via upload

---
+++ tetris/Tetrisgame.java
@@ -0,0 +1,46 @@
+package com.tetris;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Tetrisgame extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private JLabel statusbar;
+
+    public Tetris() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        statusbar = new JLabel(" 0");
+        add(statusbar, BorderLayout.SOUTH);
+
+        var board = new Board(this);
+        add(board);
+        board.start();
+
+        setTitle("Tetris");
+        setSize(200, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    JLabel getStatusBar() {
+
+        return statusbar;
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            var game = new Tetris();
+            game.setVisible(true);
+        });
+    }
+}
